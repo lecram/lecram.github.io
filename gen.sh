ccca4278caeb8d7693230c14a2022cbf4fc027ac
Marcel Rodrigues <marcelgmr@gmail.com>
2015-04-25 13:17:32 -0300

    Add timestamp.

--- gen.sh
+++ gen.sh
@@ -6,12 +6,14 @@ TEMPLATE="template.html"
 NMEFLAGS="--headernum2 --xref --html --body"
 
 TITLE=`sed -n -e 's/^[= ]*\(.*\)[= ]*$/\1/' -e '1p' $SOURCE`
+TIMESTAMP=`git log -n 1 --pretty=format:"%ai" -- $SOURCE`
 
 LINECOUNT=`wc -l < $TEMPLATE`
 SPLITLINE=`awk '/{CONTENT}/ {print FNR}' $TEMPLATE`
-TOP=`head -n $(($SPLITLINE - 1)) $TEMPLATE | sed "s/{TITLE}/$TITLE/"`
+TOP=`head -n $(($SPLITLINE - 1)) $TEMPLATE |
+     sed -e "s/{TITLE}/$TITLE/" -e "s/{TIMESTAMP}/$TIMESTAMP/"`
 BOT=`tail -n $(($LINECOUNT - $SPLITLINE)) $TEMPLATE`
-MID=`nme $NMEFLAGS < $SOURCE`
+MID=`sed '1d' $SOURCE | nme $NMEFLAGS`
 
 cat > $DEST <<EOF
 $TOP
