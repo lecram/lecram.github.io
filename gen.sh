86e6c695a20aa6328eefd9047074543f869dfd30
Marcel Rodrigues <marcelgmr@gmail.com>
2021-08-28 16:54:02 -0300

    gen: use more quotes.

--- gen.sh
+++ gen.sh
@@ -5,19 +5,19 @@ DEST="$2"
 TEMPLATE="template.html"
 NMEFLAGS="--headernum2 --xref --html --body"
 
-TITLE=`sed -n -e 's/^[= ]*\(.*\)[= ]*$/\1/' -e '1p' $SOURCE`
-if [ `git diff-files --quiet $SOURCE` ]
+TITLE=`sed -n -e 's/^[= ]*\(.*\)[= ]*$/\1/' -e '1p' "$SOURCE"`
+if [ `git diff-files --quiet "$SOURCE"` ]
 then
   TIMESTAMP=`date -d "@$(stat -c '%Y' $SOURCE)" "+%F %T %z"`
 else
-  TIMESTAMP=`git log -n 1 --pretty=format:"%ai" -- $SOURCE`
+  TIMESTAMP=`git log -n 1 --pretty=format:"%ai" -- "$SOURCE"`
 fi
 
-LINECOUNT=`wc -l < $TEMPLATE`
-SPLITLINE=`awk '/{CONTENT}/ {print FNR}' $TEMPLATE`
-TOP=`head -n $(($SPLITLINE - 1)) $TEMPLATE |
+LINECOUNT=`wc -l < "$TEMPLATE"`
+SPLITLINE=`awk '/{CONTENT}/ {print FNR}' "$TEMPLATE"`
+TOP=`head -n $(($SPLITLINE - 1)) "$TEMPLATE" |
      sed -e "s/{TITLE}/$TITLE/" -e "s/{TIMESTAMP}/$TIMESTAMP/"`
-BOT=`tail -n $(($LINECOUNT - $SPLITLINE)) $TEMPLATE`
+BOT=`tail -n $(($LINECOUNT - $SPLITLINE)) "$TEMPLATE"`
 MID=`sed '1d' $SOURCE | nme $NMEFLAGS`
 
 cat > $DEST <<EOF
